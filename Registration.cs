87396892972f4c99982f2a76ed36feb734de0a82
Lunamon1 <ikc73@hotmail.com>
2023-08-11 14:52:54 -0400

    adding code

---
+++ Registration.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace School
+{
+    class Registration
+    {
+        static void Main(string[] args)
+        {
+            Student Maria = new Student();
+            //  Maria = new Student();
+            // Student Josh = new School("Josh", 21);
+            
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace School
+{
+    public class Student
+    {
+        private string personName;
+        private int personAge;
+        private int personId;
+
+        public Student(string name, int age)
+        {
+            personName = name;
+            personAge = age;
+        }
+
+        public Student()
+        {
+            
+        }
+        public string Name
+        {
+            get { return personName; }
+            set { personName = value; }
+        }
+        public int Age
+        {
+            get { return personAge; }
+            set { personAge = value; }
+        }
+    }
+}
